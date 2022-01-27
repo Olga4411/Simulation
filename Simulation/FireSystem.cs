e94933a2cb56f8f07d874c8ae54fc01a374ca946
Novi_ij29@itstepru.onmicrosoft.com <Novi_ij29@itstepru.onmicrosoft.com>
2022-01-27 10:17:34 +0700

    1

--- Simulation/FireSystem.cs
+++ Simulation/FireSystem.cs
@@ -17,5 +17,28 @@ namespace Simulation
         // Поле сущетсвует без класса
         //Статическое поле -поле, принадлежит не объекту класса,а  в целом к классу
         public static AlarmDelegate alarm = null;
+        // метод, который симулирует срабатыывание тревоги
+        public static bool checkSmokeLevel()
+        {
+            //рандомная заглушка
+            Random random = new Random(DateTime.Now.Millisecond);
+            if (random.Next(1,5)==3)
+            {
+                // Пожар
+                makeAlarm();// вызываем оповещение
+                return true;
+            }
+            return false;
+        }
+        // методы оповещения всех подписчиков о пожаре
+        public static void makeAlarm()
+        {
+            //if(alarm !=null)// если есть кого оповещать
+            //{
+            //    alarm.Invoke();//вызов
+            //}
+            alarm?.Invoke();// Идентичный коду выше
+            //InVoke вызовет все делегаты
+        }
     }
 }
--- Simulation/Program.cs
+++ Simulation/Program.cs
@@ -8,21 +8,46 @@ namespace Simulation
 {
     class Program
     {
+        // тестирование пожарной системы
+        static void TestFireSystem()
+        {
+            // 1.создадим учителя
+            Teacher teacher = new Teacher("Борис Глебыч ");
+            // создадим студентов
+            Student[] students = new Student[]
+            {
+                new Student("Миша"),
+                new Student("Маша")
+            };
+
+            //3 подключаем наши объекты к пожарной системе
+            //3/1 подключаем учителя
+            FireSystem.alarm += teacher.Evacuation;
+            //3/2 подключаем учеников
+            foreach(Student student in students)
+            FireSystem.alarm += student.Evacuation;
+            // 4.симитируем пожар
+            while (FireSystem.checkSmokeLevel()==false)
+            {
+                Console.WriteLine($"Все нормально, дыма нет");
+            }
+            Console.WriteLine("Случился пож-р");
+        }
         static void Main(string[] args)
         {
             //сделаем симуляцию двух систем:
             //1. система пожарной трев-ги
 
-            Human human = new Teacher(" Василий Васильевич");
-            Console.WriteLine(human);
+            //Human human = new Teacher(" Василий Васильевич");
+            //Console.WriteLine(human);
 
-            human.Evacuation();
-            
+            //human.Evacuation();
 
-            human = new Student("Петя");
-            Console.WriteLine(human);
-            human.Evacuation();
 
+            //human = new Student("Петя");
+            //Console.WriteLine(human);
+            //human.Evacuation();
+            TestFireSystem();
             Console.ReadLine();
         }
     }
